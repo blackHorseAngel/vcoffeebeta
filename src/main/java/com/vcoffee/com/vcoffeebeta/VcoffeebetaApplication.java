05046838729ab6f5538c8b4ddd84e4dc453280ab
blackHorseAngel <zhangsm51@sina.cn>
2022-01-11 18:34:51 +0800

    初始化一个最基本的spring boot项目

---
+++ src/main/java/com/vcoffee/com/vcoffeebeta/VcoffeebetaApplication.java
@@ -0,0 +1,13 @@
+package com.vcoffee.com.vcoffeebeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcoffeebetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VcoffeebetaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vcoffee/com/vcoffeebeta/VcoffeebetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcoffee.com.vcoffeebeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcoffeebetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
