4978d2bf3796d7028a73ef7930c8de446b80f3f0
blackHorseAngel <zhangsm51@sina.cn>
2023-03-22 12:13:57 +0800

    增加发送带附件和带多张图片的邮件操作； 待增加接受邮件操作和发送带表格的邮件操作；

--- src/main/java/com/vcoffeebeta/util/MailUtil.java
+++ src/main/java/com/vcoffeebeta/util/MailUtil.java
@@ -2,11 +2,14 @@ package com.vcoffeebeta.util;
 
 import com.sun.mail.util.MailSSLSocketFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 import javax.mail.*;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
 import java.security.GeneralSecurityException;
 import java.util.Properties;
 
@@ -38,8 +41,8 @@ public class MailUtil {
       transport.connect("stmp.qq.com",587,"714680900","rfmcgesvyfgzbbhc");
 //          transport.connect("714680900", "rfmcgesvyfgzbbhc");
           //4、创建邮件
-          Message message = createSimpleMessage(fromMail, toMail, emailMessage, session);
-          transport.sendMessage(message, message.getAllRecipients());
+//          Message message = createSimpleMessage(fromMail, toMail, emailMessage, session);
+//          transport.sendMessage(message, message.getAllRecipients());
       }catch(NoSuchProviderException e1){
           log.error("没有获取到对应的邮箱服务器异常",e1);
           e1.printStackTrace();
@@ -59,42 +62,14 @@ public class MailUtil {
   }
 
     /**
-     * 创建邮箱发送消息
-     * @param fromMail
-     * @param toMail
-     * @param emailMessage
-     * @param session
+     * 创建邮箱发送简单消息
+     * @param
+     * @param
+     * @param
+     * @param
      * @return
      */
-    private static Message createSimpleMessage(String fromMail, String toMail, String emailMessage, Session session) {
-        //创建邮件
-        MimeMessage message = new MimeMessage(session);
-        try{
-            //指明邮件的发送人
-            message.setFrom(new InternetAddress(fromMail));
-            //指明邮件的收件人
-            message.setRecipient(Message.RecipientType.TO,new InternetAddress(toMail));
-            //设置邮件标题
-            message.setSubject("用户激活");
-            //设置邮件内容
-            message.setContent(emailMessage,"text/html;charset=utf-8");
-        }catch(AddressException e1){
-            log.error("地址异常",e1);
-            e1.printStackTrace();
-        }catch(MessagingException e2){
-            log.error("消息异常",e2);
-            e2.printStackTrace();
-        }
-      return message;
-    }
-
-    /**
-     * 邮箱发送测试
-     * @param args
-     */
-    public static void main(String[] args) throws MessagingException {
-//        boolean flag = sendEmail("1909924230","714680900","该用户已激活");
-//        System.out.println(flag);
+    public static void createSimpleMessage() throws MessagingException {
         //收件人邮箱
         String to = "1909924230@qq.com";
         //发送人邮箱
@@ -145,11 +120,164 @@ public class MailUtil {
             transport.connect("smtp.qq.com",465,from,"rfmcgesvyfgzbbhc");
             transport.send(message);
 
-        } catch (GeneralSecurityException e) {
+        } catch (GeneralSecurityException | AddressException e) {
             throw new RuntimeException(e);
-        }finally {
+        } catch (NoSuchProviderException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        } finally {
             transport.close();
         }
+    }
+
+    /**
+     * 发送带附件的邮件
+     * @throws MessagingException
+     */
+    public static void createAttachFileMessage() throws MessagingException {
+        //收件人邮箱
+        String to = "1909924230@qq.com";
+        //发送人邮箱
+        String from = "714680900@qq.com";
+        //发送邮箱主机
+        String host = "smtp.qq.com";
+        //获取系统属性
+        Properties properties = System.getProperties();
+        System.out.println("获取到的系统属性：" + properties.toString());
+        //设置邮件服务器
+        properties.setProperty("mail.smtp.host",host);
+        //设置传输协议
+        properties.setProperty("mail.transport.protocol","smtp");
+        //设置密钥
+        properties.setProperty("mail.smtp.auth","true");
+        //设置端口号为587
+        properties.setProperty("mail.smtp.socketFactory.port","587");
+
+        Transport transport = null;
+        try {
+            //对于QQ邮箱，还需要进行SSL加密
+            MailSSLSocketFactory sf = new MailSSLSocketFactory();
+            sf.setTrustAllHosts(true);
+            properties.put("mail.smtp.ssl.enable","true");
+            properties.put("mail.smtp.ssl.socketFactory",sf);
+//            properties.put("mail.smtp.ssl.protocols","TLSv1.2");
+            //获取默认的session对象
+            Session session = Session.getDefaultInstance(properties, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(from,"rfmcgesvyfgzbbhc");
+                }
+            });
+            session.setDebug(true);
+            //创建默认的MimeMessage对象
+            MimeMessage message = new MimeMessage(session);
+            //上传附件需要使用mimeMessageHelper对象
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message,true);
+            //set from 头部字段
+            mimeMessageHelper.setFrom(new InternetAddress(from));
+            //set to 头部字段
+            mimeMessageHelper.setTo(new InternetAddress(to));
+            //set subject 头字段
+            mimeMessageHelper.setSubject("test mail transporting");
+            //设置消息体
+            mimeMessageHelper.setText("set message successfully");
+            //增加附件的地址
+            mimeMessageHelper.addAttachment("test.jpg",new File("C:\\Users\\Administrator\\Desktop"));
+            //发送邮件
+            transport = session.getTransport();
+//            transport.connect("smtp.qq.com",587,"714680900@qq.com","rfmcgesvyfgzbbhc");
+            transport.connect("smtp.qq.com",465,from,"rfmcgesvyfgzbbhc");
+            transport.send(message);
+
+        } catch (GeneralSecurityException | AddressException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchProviderException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        } finally {
+            transport.close();
+        }
+    }
+
+    /**
+     * 发送多张带图片的邮件
+     * @throws MessagingException
+     */
+    public static void createMorePicturesMessage() throws MessagingException {
+        //收件人邮箱
+        String to = "1909924230@qq.com";
+        //发送人邮箱
+        String from = "714680900@qq.com";
+        //发送邮箱主机
+        String host = "smtp.qq.com";
+        //获取系统属性
+        Properties properties = System.getProperties();
+        System.out.println("获取到的系统属性：" + properties.toString());
+        //设置邮件服务器
+        properties.setProperty("mail.smtp.host",host);
+        //设置传输协议
+        properties.setProperty("mail.transport.protocol","smtp");
+        //设置密钥
+        properties.setProperty("mail.smtp.auth","true");
+        //设置端口号为587
+        properties.setProperty("mail.smtp.socketFactory.port","587");
+
+        Transport transport = null;
+        try {
+            //对于QQ邮箱，还需要进行SSL加密
+            MailSSLSocketFactory sf = new MailSSLSocketFactory();
+            sf.setTrustAllHosts(true);
+            properties.put("mail.smtp.ssl.enable","true");
+            properties.put("mail.smtp.ssl.socketFactory",sf);
+//            properties.put("mail.smtp.ssl.protocols","TLSv1.2");
+            //获取默认的session对象
+            Session session = Session.getDefaultInstance(properties, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(from,"rfmcgesvyfgzbbhc");
+                }
+            });
+            session.setDebug(true);
+            //创建默认的MimeMessage对象
+            MimeMessage message = new MimeMessage(session);
+            //上传附件需要使用mimeMessageHelper对象
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message,true);
+            //set from 头部字段
+            mimeMessageHelper.setFrom(new InternetAddress(from));
+            //set to 头部字段
+            mimeMessageHelper.setTo(new InternetAddress(to));
+            //set subject 头字段
+            mimeMessageHelper.setSubject("test mail transporting");
+            //设置消息体
+            mimeMessageHelper.setText("set message successfully");
+            //增加图片资源
+            mimeMessageHelper.addInline("p01",new FileSystemResource(new File("")));
+            mimeMessageHelper.addInline("p02",new FileSystemResource(new File("")));
+            //发送邮件
+            transport = session.getTransport();
+//            transport.connect("smtp.qq.com",587,"714680900@qq.com","rfmcgesvyfgzbbhc");
+            transport.connect("smtp.qq.com",465,from,"rfmcgesvyfgzbbhc");
+            transport.send(message);
+
+        } catch (GeneralSecurityException | AddressException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchProviderException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        } finally {
+            transport.close();
+        }
+    }
+    //引入freemarker（带表格）
+    //引入thymleaf（带表格）
+    /**
+     * 邮箱发送测试/接受邮件测试
+     * @param args
+     */
+    public static void main(String[] args) throws MessagingException {
 
     }
 }
